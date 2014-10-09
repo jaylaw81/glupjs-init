0cb02cfa09a95ca89d0f06635de16c5c5fee2beb
Jason Law <jlaw@sapient.com>
2014-10-09 13:20:46 -0400

    added dest folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,31 @@ var gulp = require('gulp')
     , watch = require('gulp-watch')
     , concat = require('gulp-concat')
     , notify = require('gulp-notify')
+    , minifyCSS = require('gulp-minify-css')
     ;
 
 gulp.task('sass', function () {
     gulp.src('./assets/sass/index.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./assets/styles'))
+        .pipe(gulp.dest('./dist/assets/styles'))
 });
 
 
 gulp.task('watch', function() {
   // watch scss files
   gulp.watch('./assets/sass/**/*.scss', function() {
-    gulp.run('sass');
+    gulp.run('sass')
   });
 
   gulp.watch('./assets/js/**/*.js', function() {
     gulp.run('js');
   });
+});
+
+gulp.task('minify-css', function() {
+  gulp.src('./assets/styles/*.css')
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('./dist/assets/styles/'))
 });
 
 gulp.task('default', ['sass', 'js', 'watch']);
